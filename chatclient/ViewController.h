2116670f53248c7c45baff0a757074448578cde9
Amit Agarwal <amitagwl@yahoo-inc.com>
2015-09-16 12:34:35 -0700

    Initial Commit

---
+++ chatclient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatclient
+//
+//  Created by Amit Agarwal on 9/16/15.
+//  Copyright (c) 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
